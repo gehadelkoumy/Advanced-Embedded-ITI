d386bde2dfb3bcc5d0e7114a22fa2d2d30e62ff6
gehadelkoumy <es-gehadgamal2023@alexu.edu.eg>
2023-09-02 01:40:02 +0300

    added drivers of stm32f4 - initial commit

---
+++ COTS_CortexM4/01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -0,0 +1,17 @@
+/*********************************************************/
+/* Author      : Gehad Elkoumy                           */
+/* Data        : 29 AUG     2023                         */
+/* Version     : V01                                     */
+/*********************************************************/
+
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define SET_BIT(VAR,BIT)          ((VAR) |= (1 << (BIT)))
+#define CLR_BIT(VAR,BIT)          ((VAR) &= ~(1 << (BIT)))
+#define GET_BIT(VAR,BIT)          ((VAR >> BIT) & 1 )
+#define TOG_BIT(VAR,BIT)          ((VAR) ^= (1 << (BIT)))
+
+
+
+#endif 
